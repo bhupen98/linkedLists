95f5c118cd8f9e9de1f19786919a6304d0ef0c7c
bhupendra thapa <b707.bt@gmail.com>
2021-02-08 01:07:21 +1000

    added some commands and still working on it

--- commands.js
+++ commands.js
@@ -5,49 +5,88 @@ const {
   SinglyLinkedList
 }  = require('./SinglyLinkedList/index');
 
-const questions = [ 
+// QUESTIONS
+//List action question
+const actionQ = [ 
   {
-    type:'input',
-    name:'push',
-    message:'Push new node'
-  },
-  {
-    type:'input',
-    name:'pop',
-    message:'Pop Node'
-  },
-  {
-    type:'input',
-    name:'shift',
-    message:'Shift Node'
-  },
-  {
-    type:'input',
-    name:'unshift',
-    message:'Unsift Node'
+    type:'list',
+    name:'action',
+    message:'choose a action you want to apply?',
+    choices:[
+      'print',
+      'push',
+      'pop',
+      'shift',
+      'unshift'
+    ]
   }
 ]
 
+//list value question
+const valQ = [
+  {
+    type:'input',
+    name:'value',
+    message:'Enter a value ?'
+  }
+]
+
+const continueQ = [
+  {
+    type:'list',
+    name:'continue',
+    message:'Do you want to apply more action to the list?',
+    choices:[
+      'Yes',
+      'No'
+    ]
+  }
+]
+
+//global variables
 var list = new SinglyLinkedList();
+
+//watch continue or not
+async function watchContinue(){
+    const continueAns = await prompt(continueQ);
+    continueAns.continue === 'Yes' ? action():null;
+}
+
+async function action(){
+  const actionAns = await prompt(actionQ);
+  if ( actionAns.action === 'print'){
+    list.printLists()
+    watchContinue();
+  }else if(actionAns.action === 'push'){
+    const valAns = await prompt(valQ)
+    //push new value
+    list.push(valAns.value);
+    //add more value or quit
+    watchContinue();
+  }else if(actionAns.action === 'pop'){
+    list.pop();
+    watchContinue()
+  }else if(actionAns.action === 'shift'){
+    list.shift();
+    watchContinue()
+  }else if(actionAns.action === 'unshift'){
+    const valAns = await prompt(valQ);
+    list.unshift(valAns.value);
+    watchContinue();
+  }
+}
 
 program
   .version('0.1.1')
   .description('datastructures and algorithms');
 
-program
-  .command('list')
-  .alias('l')
-  .description('list all the data')
-  .action(() => {
-    list.printLists() 
-  })
 
 program 
   .command('list')
   .alias('l')
   .description('manipulate lists')
-  .action( async() => {
-    const answer = await prompt(questions)
+  .action(() => {
+    action()
   })
 
 
